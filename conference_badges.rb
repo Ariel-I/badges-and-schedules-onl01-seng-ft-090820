6b710f20bb74afcfc553960ce5b12e0838aacc9c
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-11 20:46:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,13 +7,13 @@ end
 def batch_badge_creator(names= [Edsger, Ada, Charles, Alan, Grace, Linus, Matz])
   greetings = [] 
   names.each do |name| 
-    greetings <<  badge_maker(name)#
+    greetings <<  badge_maker(name)
   end
   return greetings 
 end
 
 
-def assign_rooms(speakers)
+def assign_rooms(speakers=[Edsger, Ada, Charles, Alan, Grace, Linus, Matz])
   greet = []
   speakers.each_with_index{ |speakers, index| greet << "Hello, #{speakers}! You'll be assigned to room #{index+1}!"}
   return greet
