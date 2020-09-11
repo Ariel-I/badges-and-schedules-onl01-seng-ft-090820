5d7acf13e05f2c3723157bbc02473e46f14880d8
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-11 20:07:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,10 +1,14 @@
-name = []
+name = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
 
 def badge_maker(name)
+  name.each do 
   puts "Hello, my name is #{name}."
+  break name.last-1
+ end
 end
 
-def batch_badge_creator(attendees)
+def batch_badge_creator(name)
+  
 end
 
 def assign_rooms
