f974419020d082c5a47c8bf4a11f0b8ec654e14a
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-11 20:34:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,14 +1,19 @@
+array=[]
+
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(names)
-  greetings = [] # initialize greetings as an empty array
-  names.each do |name| # for each name in the names array
-    greetings <<  badge_maker(name)# add a greeting for that name
+  greetings = [] 
+  names.each do |name| 
+    greetings <<  badge_maker(name)#
   end
-  return greetings # return the array of all greetings, at the end
+  return greetings 
 end
+
+
+
 
 def assign_rooms(speakers)
   greet = []
